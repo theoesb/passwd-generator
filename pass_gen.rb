27d7498b33f04b7f9317fffdce73ed1a1facbbea
theo eisenbart <theo.eisenbart@outlook.fr>
2023-03-07 18:33:35 +0100

    Add var

--- pass_gen.rb
+++ pass_gen.rb
@@ -1,5 +1,4 @@
 #!/bin/ruby
-
 def generator (length, difficulty)
     basic_passwd = ("a".."z").to_a + ("A".."Z").to_a
     medium_passwd = ("0".."9").to_a + basic_passwd
@@ -17,4 +16,19 @@ def generator (length, difficulty)
     end
 
     passwd = Array.new(length) { chars.sample }.join
+end
+
+print "Enter the number of passwords to generate: "
+num = gets.chomp.to_i
+
+print "Enter the length of the passwords: "
+length = gets.chomp.to_i
+
+print "Enter the difficulty of the passwords (basic, medium, strong): "
+difficulty = gets.chomp.to_sym
+puts "\n"
+puts "List of passwords:"
+
+num.times do
+  puts generator(length, difficulty)
 end
